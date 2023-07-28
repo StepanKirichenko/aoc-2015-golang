4c55544dc4059a8d08559a2d1af94918ddb0e0a9
StepanKirichenko <kirichenkostepan@yandex.ru>
2023-07-28 19:31:51 +0300

    solve day01

--- day01/day01.go
+++ day01/day01.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-    fmt.Println("hello from day01")
+	f, err := os.ReadFile("day01/input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+    fmt.Println("Part One:", partOne(f))
+    fmt.Println("Part Two:", partTwo(f))
+}
+
+const (
+	UP   = byte('(')
+	DOWN = byte(')')
+)
+
+func partOne(input []byte) int {
+	floor := 0
+
+	for _, dir := range input {
+		switch dir {
+		case UP:
+			floor += 1
+		case DOWN:
+			floor -= 1
+		}
+	}
+
+	return floor
+}
+
+func partTwo(input []byte) int {
+    floor := 0
+
+    for i, dir := range input {
+        switch dir {
+        case UP:
+            floor += 1
+        case DOWN:
+            floor -= 1
+            if floor < 0 {
+                return i + 1
+            }
+        }
+    }
+
+    return 0
 }
